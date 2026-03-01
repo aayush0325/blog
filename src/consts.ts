75870387d83dcca89b01baa959485662084058e6
Aayush Khanna <ayush25khanna@gmail.com>
2026-03-01 12:52:21 +0530

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Aayush's blog";
+export const SITE_DESCRIPTION = "My corner of the internet :)";
+export const SITE_AUTHOR = "aayush0325";
